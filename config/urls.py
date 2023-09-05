99587e730b96f761d4b87ba47cd469f27e973819
bini59 <myuni0234@gmail.com>
2023-09-05 19:40:32 +0900

    fix : url 이동 안되는 오류 수정

--- config/urls.py
+++ config/urls.py
@@ -17,7 +17,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+
+from lily import views
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('_auth/', include('_auth.urls')),
+    path('', views.index, name='index'),
 ]
