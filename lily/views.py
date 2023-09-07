33ea4baebf7e9c0e8e601f3c977e5ffb778802d4
bini59 <myuni0234@gmail.com>
2023-09-07 11:28:25 +0900

    fix : 글작성 로직 수정
    - 입력하지 않은 항목 있을 시 다시 입력

--- lily/views.py
+++ lily/views.py
@@ -12,15 +12,23 @@ def index(request):
 
 def post_POST(request):
     post = Post()
-    print(request.FILES['image'])
-    post.title = request.POST['title']
-    post.content = request.POST['content']
-    post.author = request.user
-    post.created_at = timezone.datetime.now()
-    post.image = request.FILES['image']
-    post.save()
+    try:
+        if request.POST['title'] == "" or request.POST['content'] == "":
+            raise Exception()
+        
+        post.title = request.POST['title']
+        post.content = request.POST['content']
+        post.author = request.user
+        post.created_at = timezone.datetime.now()
+        post.image = request.FILES['image']
+        post.save()
+        
+        return redirect('/')
+    except:
+        return render(request, 'lily/post.html', {'form': request.POST})
     
-    return redirect('/')
+    
+    
 
 def post_GET(request):
     return render(request, 'lily/post.html')
